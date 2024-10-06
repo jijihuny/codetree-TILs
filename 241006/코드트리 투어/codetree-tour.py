117406c5a3ad56594c2c126d24ed1b4f964d1cfb
이지훈 <112816117+jijihuny@users.noreply.github.com>
2024-10-06 20:48:52 +0900

    Title: 코드트리 투어, Time: 152ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 241006/코드트리 투어/codetree-tour.py
+++ 241006/코드트리 투어/codetree-tour.py
@@ -1,4 +1,7 @@
 from heapq import heappush as push, heappop as pop, heapify
+import sys
+input = sys.stdin.readline
+INF = float('inf')
 graph = []
 cost = []
 revenue_queue = []
@@ -6,18 +9,19 @@ product = {}
 # O(VlogV + ElogV)
 def shortest_path(start):
     global graph, cost
-    cost = [float('inf')] * len(graph)
+    cost = [INF] * len(graph)
     cost[start] = 0
     heap = []
     for adjacent, distance in graph[start].items():
         cost[adjacent] = min(cost[adjacent], distance)
         push(heap, (distance, adjacent))
-    visited = set({start})
+    visited = [False] * len(graph)
+    visited[start] = True
     while heap:
         shortest, node = pop(heap)
-        if node in visited:
+        if visited[node]:
             continue
-        visited |= {node}
+        visited[node] = True
         for adjacent, distance in graph[node].items():
             cost[adjacent] = min(cost[adjacent], shortest + distance)
             push(heap, (cost[adjacent], adjacent))
@@ -26,23 +30,25 @@ def construct(n, m, edges):
     global graph
     graph = [{} for i in range(n+1)]
     for v, u, w in edges:
-        graph[v][u] = min(w, graph[v][u] if u in graph[v] else float('inf'))
-        graph[u][v] = min(w, graph[u][v] if v in graph[u] else float('inf'))
+        graph[v][u] = min(w, graph[v][u] if u in graph[v] else INF)
+        graph[u][v] = min(w, graph[u][v] if v in graph[u] else INF)
 
     shortest_path(0)
-delete_queue = set()
 
+# O(logC)
 def create_revenue(id, revenue, dest):
     global cost, product, delete_queue
     product[id] = (revenue, dest)
-    if revenue >= cost[dest]:
+    if revenue < cost[dest]:
         push(revenue_queue, (cost[dest] - revenue, id))
 
+# O(logP)
 def delete_revenue(id):
     global product
     if id in product:
         del product[id]
 
+# O(logC)
 def selling():
     global revenue_queue, product
     id = -1
@@ -56,19 +62,21 @@ def selling():
         del product[id]
     return id
 
+# O(VlogV + ElogE + ClogC)
 def change_start(start):
     global revenue_queue, cost, product
     revenue_queue = []
     shortest_path(start)
 
     for id, (revenue, dest) in product.items():
-        if revenue >= cost[dest]:
+        if revenue < cost[dest]:
             revenue_queue += [(cost[dest]  - revenue, id)]
     
     heapify(revenue_queue)
 
 
 Q = int(input().replace(' ', ''))
+from collections import deque
 
 for i in range(Q):
     t, *command = input().split(' ')
@@ -76,8 +84,9 @@ for i in range(Q):
     if t == '100':
         n, m, *edges = command
         arr = []
+        edges = deque(edges)
         while edges:
-            u, v, w, *edges = edges
+            u, v, w = edges.popleft(), edges.popleft(), edges.popleft()
             arr += [(int(u), int(v), int(w))]
         construct(int(n), int(m), arr)
         # print(graph)
